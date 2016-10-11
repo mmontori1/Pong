caa711235d7028c42efeee323e3130b3faf8e2cd
Mariano <mmontori@umich.edu>
2016-10-10 23:41:18 -0400

    started project
    organized/created game objects + created the player script

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public GameObject player;
+	public Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+		player = this.gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
